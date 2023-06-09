ece6a86f0ff10431033a7f7c9bc9f72b50d065aa
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2023-06-09 10:38:08 -0500

    fixed endpoint health callers

--- app/api/Salud/tramites/atencionSalud/insertar_atencionsalud.ts
+++ app/api/Salud/tramites/atencionSalud/insertar_atencionsalud.ts
@@ -5,9 +5,9 @@ const endpoint : string = 'insertar_atencionsalud';
 export const apiInsertarAtencionSalud = {
 
     postInsertarAtencionSalud : function (user_id : number, date : string, kind : string) {
-        return instance.post(`${endpoint}/${user_id}`, {
+        return instance.post(`${endpoint}/${user_id}`, {params : {
             fecha : date,
             tipo : kind
-        })
+        }})
     }   
 }
