d5cd33b66fe17eea04f66aac46b56f6aed427a19
Javier Tarazona <96489034+JavierTarazona06@users.noreply.github.com>
2023-06-09 01:00:59 -0500

    Front_JAvier

--- app/api/GestionFomento/convocatorias/conv_fomento_emprendimiento.ts
+++ app/api/GestionFomento/convocatorias/conv_fomento_emprendimiento.ts
@@ -2,9 +2,8 @@ import { instance } from "../../base.api";
 
 const endpoint : string = 'conv_fomento_emprendimiento'
 
-export const getCgfEmprendimiento = {
-    
-    getCgfEmprendimientoNombre : function (user_id : number, name : string, topic : string){
+export const apiCgfEmprendimiento = {
+    getCgfEmprendimiento : function (user_id : number, name : string, topic : string){
         return instance.get(`${endpoint}/${user_id}`, {params : {
             nombre : name,
             tema : topic
