20b0b3b4a290926a7dfcede95fe724b3ad71eee0
Javier Tarazona <96489034+JavierTarazona06@users.noreply.github.com>
2023-06-09 12:01:01 -0500

    Endpoints Javier en Front

---
+++ app/api/General/est_toma_conv.ts
@@ -0,0 +1,13 @@
+import { instance } from "../base.api";
+
+const endpoint : string = 'est_toma_conv'
+
+export const apiEst_toma_conv = {
+    post_est_toma_conv: function (usuario_id:number, convoc_id:number, date:string){
+        return instance.post(`${endpoint}/${usuario_id}`, {params : {
+            est_id: usuario_id,
+            conv_id: convoc_id,
+            fecha:  date
+        }})
+    }
+}
