7d456df61189085c78c459222ee5ef5400159e03
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2023-06-08 22:27:53 -0500

    schedule and cancel appointments first implementation

--- app/pages/salud/servicios/gestion/page.tsx
+++ app/pages/salud/servicios/gestion/page.tsx
@@ -7,9 +7,68 @@ import { Grid, TextField, Button } from '@mui/material';
 import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { BaseForm } from '@/app/components/General/BaseForm';
+import { formGestionCita } from '@/app/types/salud/servicios/gestion/formsGestion';
+import { apiAgendarCitaMedica } from '@/app/api/Salud/servicios/gestion/agendar_cita_medica';
+import { apiCancelarCitaMedica } from '@/app/api/Salud/servicios/gestion/cancelar_cita_medica';
 // AMBAS FUNCIONES USAN *****PUT*****
 // PARÁMETROS : user_id : number;
 export default function GestionarCitas() {
+
+    //1. Toma de datos formularios
+    const [paramsAgendarCita, setParamsAgendarCita] = React.useState<formGestionCita>({
+        cedula: 0,
+        fecha: '',
+        especialidad: ''
+    })
+
+    const [paramsCancelarCita, setParamsCancelarCita] = React.useState<formGestionCita>({
+        cedula: 0,
+        fecha: '',
+        especialidad: ''
+    })
+
+    //2. Manejo de las entradas, es aqui donde alteramos los estados del paso anterior.
+    const handleChange1 = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setParamsAgendarCita({
+            ...paramsAgendarCita, [e.target.name]: e.target.value
+        })
+    }
+
+    const handleChange2 = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setParamsCancelarCita({
+            ...paramsCancelarCita, [e.target.name]: e.target.value
+        })
+    }
+
+    //3. Manejo del formulario, este debe hacer la request para que se actualicen los datos.
+    const handleActualizacion = (e: React.FormEvent<HTMLFormElement>) => {
+
+        //Se detiene el comportamiendo por defecto del formulario.
+        e.preventDefault();
+
+        //Llamamos a las funciones que golpean los endpoint
+        apiAgendarCitaMedica.putAgendarCita(paramsAgendarCita.cedula, paramsAgendarCita.fecha, paramsAgendarCita.especialidad).then((response) => {
+            console.log(response.data)
+        }).catch((error) => {
+            console.log(`${error}: hubo un error actualizando`)
+        })
+    }
+
+    const handleCancelacion = (e: React.FormEvent<HTMLFormElement>) => {
+        
+        //Se detiene el comportamiendo por defecto del formulario.
+        e.preventDefault();
+
+        //Llamamos a las funciones que golpean los endpoint
+        apiCancelarCitaMedica.putCancelarCita(paramsCancelarCita.cedula, paramsCancelarCita.fecha, paramsCancelarCita.especialidad).then((response) => {  
+            console.log(response.data)
+        }).catch((error) => {
+            console.log(`${error}: hubo un error actualizando`)
+        })
+
+    }
+
+
 
     //Schedule appointments
     const [scheduleApointment, setscheduleApointment] = React.useState<null>(null);
@@ -17,17 +76,6 @@ export default function GestionarCitas() {
     //Cancel appointments
     const [cancelAppointment, setcancelAppointment] = React.useState<null>(null);
 
-    const handleSubmit1 = (e: React.FormEvent<HTMLFormElement>) => {
-
-        e.preventDefault();
-        //endpoint function here
-    }
-
-    const handleSubmit2 = (e: React.FormEvent<HTMLFormElement>) => {
-
-        e.preventDefault();
-        //endpoint function here
-    }
 
     return (
         <LayoutSalud>
@@ -49,14 +97,11 @@ export default function GestionarCitas() {
 
                     <BaseForm title='Agendar una cita' children={
                         <>
-                            <TextField placeholder='Cédula' />
+                            <TextField name='cedula' onChange={handleChange1} placeholder='Cédula' />
 
-                            <TextField placeholder='fecha' />
-                            {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                <DatePicker format="YYYY-MM-DD" />
-                            </LocalizationProvider> */}
+                            <TextField name='fecha' onChange={handleChange1} placeholder='fecha' />
 
-                            <TextField placeholder='Especialidad' />
+                            <TextField name='especialidad' onChange={handleChange1} placeholder='Especialidad' />
 
                         </>
                     }
@@ -94,7 +139,7 @@ export default function GestionarCitas() {
                                     ) : null}
                             </>
                         }
-                        submit={handleSubmit1}
+                        submit={handleActualizacion}
                     />
 
                 </Grid>
@@ -104,16 +149,11 @@ export default function GestionarCitas() {
 
                     <BaseForm title='Cancelar una cita' children={
                         <>
-                            <TextField placeholder='Cédula' />
+                            <TextField name='cedula' onChange={handleChange2} placeholder='Cédula' />
 
-                            <TextField placeholder='fecha' />
-                            {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                <DatePicker format="YYYY-MM-DD" />
-                            </LocalizationProvider> */}
+                            <TextField name='fecha' onChange={handleChange2} placeholder='fecha' />
 
-                            <TextField placeholder='Especialidad' />
-
-
+                            <TextField name='especialidad' onChange={handleChange2} placeholder='Especialidad' />
 
                         </>
                     }
@@ -151,7 +191,7 @@ export default function GestionarCitas() {
                                     ) : null}
                             </>
                         }
-                        submit={handleSubmit2}
+                        submit={handleCancelacion}
                     />
 
 
