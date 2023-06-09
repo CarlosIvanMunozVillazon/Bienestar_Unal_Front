5cbce930b3a50ff9d34a12ae2ae45a8adc838f6f
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2023-06-09 10:06:13 -0500

    update salud put request

--- app/api/Salud/tramites/atencionSalud/modificar_atencionsalud.ts
+++ app/api/Salud/tramites/atencionSalud/modificar_atencionsalud.ts
@@ -4,8 +4,9 @@ const endpoint = 'modificar_atencionsalud'
 
 export const apiModificarAtencionSalud = {
 
-    putModificarAtencionSalud : function (atencionsalud_id : number, date : string, kind : string) {
-        return instance.put(`${endpoint}/${atencionsalud_id}`, null, {params : {
+    putModificarAtencionSalud : function (saludatencion_id : number, date : string, kind : string) {
+        return instance.put(`${endpoint}`, null, {params : {
+            atencionsalud_id : saludatencion_id,
             fecha : date,
             tipo : kind
         }})
