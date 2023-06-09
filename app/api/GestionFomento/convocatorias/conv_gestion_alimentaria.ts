5ba26037e4ed442703c312ed9aca15da48b0236a
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2023-06-09 00:18:38 -0500

    atencionSalud section finished without test

--- app/api/GestionFomento/convocatorias/conv_gestion_alimentaria.ts
+++ app/api/GestionFomento/convocatorias/conv_gestion_alimentaria.ts
@@ -2,7 +2,7 @@ import { instance } from "../../base.api";
 
 const endpoint : string = 'conv_gestion_alimentaria'
 
-export const getCgfAlimentaria = {
+export const apiCgfAlimentaria = {
     getCgfAlimentariaMultiple : function (user_id:number, comida : string , lugar : string) {
         return instance.get(`${endpoint}/${user_id}/${comida}/${lugar}`)
     },
