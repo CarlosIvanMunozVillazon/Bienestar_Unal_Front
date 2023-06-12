dc08b2bf96caf39990024ab76c3986f44c6a459e
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2023-06-12 14:25:39 -0500

    sports section fully implemented

--- app/api/Deporte/otros/eventoTaller.ts
+++ app/api/Deporte/otros/eventoTaller.ts
@@ -5,7 +5,7 @@ const endpoint : string = 'taller'
 export const apiEventoTaller = {
 
     getEventoTaller : (idEventoTaller : number) => {
-        return instance.get(endpoint , {params : {
+        return instance.get(`${endpoint}` , {params : {
             id_eve_ta : idEventoTaller
         }})
     }
